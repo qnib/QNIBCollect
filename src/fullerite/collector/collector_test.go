b77a2ffb4d2f5011e360097801163edc2f3339a9
Christian Kniep <christian@qnib.org>
2016-05-14 10:22:02 +0200

    reduced collectors

--- src/fullerite/collector/collector_test.go
+++ src/fullerite/collector/collector_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	names := []string{"Test", "Diamond", "Fullerite", "ProcStatus", "ProcStatus Instance2"}
+	names := []string{"Test", "Diamond", "Fullerite"}
 	for _, name := range names {
 		c := New(name)
 		name = strings.Split(name, " ")[0]
