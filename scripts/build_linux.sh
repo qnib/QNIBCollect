c2ce6efaa77b9c121259ea30d0bb80b4579c202c
Christian Kniep <ckniep@gaikai.com>
2016-05-18 15:06:53 +0200

    add build script

---
+++ scripts/build_linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+TAG=$(git describe --abbrev=0 --tags)
+if [ ! -z $1 ];then
+   TAG=$1
+fi
+
+PDIR=$(echo $(pwd) |sed -e 's/scripts//')
+docker run -ti -v ${PDIR}:/data/ -w /data/ qnib/golang make
+mv ${PDIR}/bin/fullerite ${PDIR}/bin/fullerite-${TAG}-Linux
+rm -f bin/gom bin/beatit
+
+docker run -ti -v ${PDIR}:/data/ -w /data/ qnib/alpn-go-dev make
+mv ${PDIR}/bin/fullerite ${PDIR}/bin/fullerite-${TAG}-LinuxMusl
+rm -f ${PDIR}/bin/gom bin/beatit
+
